26b93ff273c14a1aa382b4aa284b665ba35fc653
break1st <brenoalvees445@gmail.com>
2026-03-10 11:36:55 -0300

    03 - criando o findById

--- src/main/java/br/ifmg/produto1_2026/resources/CategoriaResource.java
+++ src/main/java/br/ifmg/produto1_2026/resources/CategoriaResource.java
@@ -1,11 +1,13 @@
 package br.ifmg.produto1_2026.resources;
 
+import br.ifmg.produto1_2026.dto.CategoriaDTO;
 import br.ifmg.produto1_2026.entities.Categoria;
 import br.ifmg.produto1_2026.services.CategoriaService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +20,13 @@ public class CategoriaResource {
     @Autowired
     private CategoriaService categoriaService;
 
+    @GetMapping("/{id}")
+    public ResponseEntity<CategoriaDTO> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(categoriaService.findById(id));
+    };
+
     @GetMapping
-    public ResponseEntity<List<Categoria>> findAll() {
+    public ResponseEntity<List<CategoriaDTO>> findAll() {
         return ResponseEntity.ok().body(categoriaService.findAll());
     };
 }
